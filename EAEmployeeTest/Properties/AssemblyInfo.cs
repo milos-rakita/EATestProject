b310101646d22e630196de120114ad8f17f15811
Milos Rakita <milos-rakita@users.noreply.github.com>
2018-05-04 21:41:40 +0200

    t

---
+++ EAEmployeeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EAEmployeeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EAEmployeeTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("be9561b5-b38c-4987-a4d8-c62e818b44c6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- EATestProject/Base/Browser.cs
+++ EATestProject/Base/Browser.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EATestProject
+namespace EAAutoFramework.Base
 {
-    public class Class1
+    class Browser
     {
     }
 }
